e329354f4794c3a79e56f5ebc25f9e918f8671bc
EstrellaDionis <EstrellaDionis@gmail.com>
2021-08-25 15:16:43 -0400

    using a function to return a coinflip result

---
+++ Coin_Flip.js
@@ -0,0 +1,17 @@
+function flip(){
+
+    for (let i = 0; i<10; i++){
+
+    var coinFlip = Math.round(Math.random());
+
+    if (coinFlip === 1){
+        console.log("heads");
+    }
+    if (coinFlip === 0){
+        console.log("tails");
+
+    }
+    }
+}
+
+flip();
