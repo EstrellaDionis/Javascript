63365235a1d0a98847d1ecf0ab010a6fd259c601
EstrellaDionis <EstrellaDionis@gmail.com>
2021-10-10 20:13:34 -0400

    program to show mutual friends

---
+++ Mutual_Friends_List.js
@@ -0,0 +1,11 @@
+var friends = "Mutual friends:";
+friends = friends + " Jan,";
+friends = friends + " Brett,";
+friends = friends + " Carly,";
+friends = friends + " Dionis,";
+friends = friends + " Kaitlyn,";
+friends = friends + " Mark,";
+friends = friends + " Brian,";
+friends = friends + " Anauris";
+
+console.log(friends);
